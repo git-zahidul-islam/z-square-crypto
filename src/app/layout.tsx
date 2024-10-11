4f5fa45b92a68dd253772b15120c9b7359734e46
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-11 16:23:34 +0600

    start blog website with typescript next.js shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
+import { ThemeProvider } from "@/context/themeConext";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Z-Square Crypto",
+  description: "Z-Square Crypto is a crypto currency news update website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <Navbar />
+          <main className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Banner from "@/components/shared/Banner";
+import NewsCard from "@/components/shared/NewsCard";
+import NewsLetter from "@/components/shared/NewsLetter";
+import { NewsItem } from "@/types/news";
+
+const Home = async() => {
+  const data = await fetch('https://news-api-next-js-kappa.vercel.app/api/news', {
+    cache: 'force-cache'
+  });
+  const news = await data.json();
+  
+  return (
+    <div className="py-12">
+        <Banner/>
+
+        <div className="my-12">
+          <h2 className="text-2xl font-bold mb-8">Latest News</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {
+              news.slice(0, 3).map((item: NewsItem) => (
+                <NewsCard key={item?._id} item={item}/>
+              ))
+            }
+          </div>
+        </div>
+
+        <NewsLetter/>
+    </div>
+  );
+}
+
+export default Home
